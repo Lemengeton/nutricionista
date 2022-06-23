f29f601065478193717926cb73930a9c7d40e804
joaquin chamorro <joaquin.ch1736@gmail.com>
2022-06-23 17:36:53 -0300

    la base de datos tampoco tiene acento ahora

--- recetas.sql
+++ recetas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2022 a las 06:00:41
+-- Tiempo de generación: 23-06-2022 a las 22:35:31
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -31,7 +31,7 @@ CREATE TABLE `recetas` (
   `id` int(11) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `tiempo` varchar(20) NOT NULL,
-  `descripción` varchar(100) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
   `imagen` varchar(100) NOT NULL,
   `calorias` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,29 +40,8 @@ CREATE TABLE `recetas` (
 -- Volcado de datos para la tabla `recetas`
 --
 
-INSERT INTO `recetas` (`id`, `nombre`, `tiempo`, `descripción`, `imagen`, `calorias`) VALUES
-(1, '', '20-10min', 'una receta echa de caca', '', ''),
+INSERT INTO `recetas` (`id`, `nombre`, `tiempo`, `descripcion`, `imagen`, `calorias`) VALUES
 (4, 'atawt', '123gaf', 'aftgagag', 'images/logo.png', '');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `DNI` int(50) NOT NULL,
-  `nombre` text NOT NULL,
-  `apellido` text NOT NULL,
-  `contraseña` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`DNI`, `nombre`, `apellido`, `contraseña`) VALUES
-(44962456, 'Aleja', 'bug', '123456');
 
 --
 -- Índices para tablas volcadas
@@ -73,12 +52,6 @@ INSERT INTO `usuario` (`DNI`, `nombre`, `apellido`, `contraseña`) VALUES
 --
 ALTER TABLE `recetas`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`DNI`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
