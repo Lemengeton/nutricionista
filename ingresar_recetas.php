b5eecbc643fdc684d81178bef32e2c0611e427ed
unknown <joaquin.ch1736@gmail.com>
2022-06-23 01:16:28 -0300

    hice cambios en la carga

--- ingresar_recetas.php
+++ ingresar_recetas.php
@@ -24,14 +24,14 @@
 <body class="homepage">
 	<?php include ('header.php') ?>
 	<div class="hero">
-		<div class="container ">
+		<div class="container recipe-section">
 			<img src="./images/logo_rework.svg" class="icon-logo" width="150"/>
             <h1 class="site-title">ingresar recetas</h1>
             <form method="POST" enctype="multipart/form-data" action="backend/subir_recetas.php">
-            	<input type="text" name="receta" placeholder="Nombre receta" class="mb-2"> <br>
-            	<input type="text" name="tiempo" placeholder="Tiempo" class="mb-2"> <br>
-            	<textarea name="descripcion" placeholder="descripcion de receta" class=""></textarea><br>
-            	<input accept="image/*" type="file" name="imagen"><br>
+            	<input type="text" name="receta" placeholder="Nombre receta" class="mb-2 input"> <br>
+            	<input type="text" name="tiempo" placeholder="Tiempo" class="mb-2 input"> <br>
+            	<textarea name="descripcion" placeholder="descripcion de receta" class="mb-2"></textarea><br>
+            	<input class="imginput mb-2" accept="image/*" type="file" name="imagen"><br>
             	<button class="button mx-auto">Subir</button>
             </form>
         </div>
