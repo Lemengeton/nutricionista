dae04c8cf39837a93e81f6686a939f14c3f2a5cd
Alespiderpro <ale.cobra.jr@gmail.com>
2022-07-31 13:59:19 -0300

    sigo arreglando

--- header_index.php
+++ header_index.php
@@ -23,9 +23,15 @@
                 <li class="menu-item"><a href="offer.php">Servicios</a></li>
                 <li class="menu-item"><a href="recipe.php">Recetas</a></li>
                 <li class="menu-item"><a href="contacto.php">Contacto</a></li>
-                <li class="menu-item"><a href="login.php">Login</a></li>
-                <li class="menu-item"><a href="ingresar_recetas.php">Subir recetas</a></li>
-                <li class="menu-item"><a href="backend/cerrar_sesion.php">cerrar sesion</a></li>
+                <?php 
+                session_start();
+                if(isset($_SESSION['DNI_Usuario']))
+                {
+                    echo '<li class="menu-item"><a href="ingresar_recetas.php">Subir recetas</a></li>
+                <li class="menu-item"><a href="backend/cerrar_sesion.php">cerrar sesion</a></li>';
+                }else{
+                    echo '<li class="menu-item"><a href="login.php">Login</a></li>';
+                } ?>
             </ul> <!-- .menu -->
         </div> <!-- .main-navigation -->
 
