ddc2fcd63f20033fef220fc0310e8f28818964ed
Alespiderpro <ale.cobra.jr@gmail.com>
2022-07-31 14:02:05 -0300

    nose

--- ingresar_recetas.php
+++ ingresar_recetas.php
@@ -1,17 +1,11 @@
-<?php 
-	session_start();
-	if(isset($_SESSION['DNI_Usuario']))
-	{	
-	}else{
-		header("Location:index.php");
-	}
-	?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Nutricionista | Recetas</title>
+	<title>Nutricionista | recetas</title>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
@@ -22,19 +16,12 @@
 		
 </head>
 <body class="homepage">
-<<<<<<< HEAD
-	<?php include ('header.php') ?>
-=======
 	<?php include ('header.php');
-	include ('backend/conexion.php');
 	if(isset($_SESSION['DNI_Usuario']))
 	{	
 	}else{
 		header("Location:index.php");
-	}
-	 ?>
-
->>>>>>> ocultar-accesos
+	} ?>
 	<div class="hero">
 		<div class="container recipe-section">
 			<img src="./images/logo_rework.svg" class="icon-logo" width="150"/>
@@ -44,12 +31,10 @@
             	<input type="text" name="tiempo" placeholder="Tiempo" class="mb-2 input"> <br>
             	<input type="text" name="calorias" placeholder="Calorias" class="mb-2 input"> <br>
             	<textarea name="descripcion" placeholder="descripcion de receta" class="mb-2"></textarea><br>
+            	<textarea name="cocinado" placeholder="metodo de cocinado" class="mb-2"></textarea><br>
             	<input class="imginput mb-2" accept="image/*" type="file" name="imagen"><br>
-<<<<<<< HEAD
-=======
-            	<input id="myInput" class="mb-2 input" type="number" placeholder="Cantidad de ingredientes">
+            	<input id="myInput" class="mb-2 input" type="number" placeholder="Cantidad de ingredientes"><br>
             	<div class="row" id="content"></div>
->>>>>>> ocultar-accesos
             	<button class="button mx-auto">Subir</button>
             </form>
         </div>
@@ -57,12 +42,18 @@
     <?php
 	include 'footer.html';
 	?>
-<<<<<<< HEAD
-=======
 	<script type="text/javascript">
 		$(function(){
+
+  $("#mySelect").change(function(){
+  var cantidad =$("#mySelect").val();
+    $("#myInput").val(cantidad);
+    RenderInputs(cantidad);
+  });
+  
   $("#myInput").change(function(){
   var cantidad = $("#myInput").val();
+    $("#mySelect").val(cantidad);
     RenderInputs(cantidad);
   });
 
@@ -80,6 +71,5 @@ $('#content').html('');
 
 }
 	</script>
->>>>>>> ocultar-accesos
 </body>
 </html>
