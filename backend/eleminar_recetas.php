1878eb827caac135173de4f3f221d9e7bab94248
Alespiderpro <ale.cobra.jr@gmail.com>
2022-08-03 23:29:03 -0300

    boton de eliminar completo

--- backend/eleminar_recetas.php
+++ backend/eleminar_recetas.php
@@ -2,5 +2,11 @@
 include ('conexion.php');
 $recetas = $_POST['receta'];
 
-$sql = "DELETE FROM `recetas` WHERE `id` = ".$recetas."
+$sql = "DELETE FROM `recetas` WHERE `id` = ".$recetas."";
+mysqli_query($conexion,$sql);
+
+$sql = "DELETE FROM `ingredientes` WHERE `ID_receta` = ".$recetas."";
+mysqli_query($conexion,$sql);
+
+header('location:../ingresar_recetas.php');
  ?>
